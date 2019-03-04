e744ced1b7a5320292c5493f33a0c6469013cfd3
Rodrigo Debian <rodrigo.jimenez@ul.com>
2019-03-04 11:51:22 -0600

    adding generic batch

--- core/TaskRunner/Pool.fs
+++ core/TaskRunner/Pool.fs
@@ -18,8 +18,8 @@ type Pool<'input, 'output> (queueToken: CancellationToken, task: PoolTask<'input
             async {
                 let! (source, input, channel) = inbox.Receive();
                 try
-                    let x = Task.Execute.Invoke(input, source)
-                    channel.Reply(Success x)
+                    let result = Task.Execute.Invoke(input, source)
+                    channel.Reply(Success result)
                     do! loop()
                 with
                 | :? OperationCanceledException as ex ->
@@ -51,13 +51,12 @@ type Pool<'input, 'output> (queueToken: CancellationToken, task: PoolTask<'input
         this.EnqueueTimeout input -1 // infinite timeout
 
     member this.EnqueueBatchTimeout list batch timeoutms =
-        let _itSource = new CancellationTokenSource();
         let items = list |> Seq.map(fun it -> this.EnqueueTimeout it timeoutms)
         let arr = Async.Parallel items
         Async.StartWithContinuations(arr,
             (fun  result -> batch.Complete.Invoke(result) |> ignore),
             (fun     err -> batch.Error.Invoke(err) |> ignore),
-            (fun     can -> batch.Cancel.Invoke(can) |> ignore), _itSource.Token)
+            (fun     can -> batch.Cancel.Invoke(can) |> ignore), GlobalToken)
 
     member this.EnqueueBatch list batch =
         this.EnqueueBatchTimeout list batch -1
